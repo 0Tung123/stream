050f0102af294a744e20e5521ce941b6bab2a70a
0Tung123 <harrybother356@gmail.com>
2025-01-25 00:44:30 +0700

    commit config

---
+++ src/modules/auth/account/account.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+
+import { AccountResolver } from './account.resolver'
+import { AccountService } from './account.service'
+
+@Module({
+	providers: [AccountService, AccountResolver]
+})
+export class AccountModule {}
